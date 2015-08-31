415e8dc567bbd81f17170d425291c1b32ce7bb59
jcshiau <jcshiau@gmail.com>
2015-08-30 20:51:38 -0700

    implemented Devise for users

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -76,4 +76,8 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  #Required for Heroku
+  # Note to set this to your own host name
+  config.action_mailer.default_url_options = { host: 'culturhaus.herokuapp.com' }
 end
