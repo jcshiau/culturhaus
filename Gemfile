0f02f7e1105b47ea33d95d9d796f7fc9d30261b4
jcshiau <jcshiau@gmail.com>
2015-09-01 16:29:11 -0700

    added paperclip to be able to upload images

--- Gemfile
+++ Gemfile
@@ -11,6 +11,7 @@ gem 'jbuilder', '~> 2.0'
 gem 'sdoc', '~> 0.4.0', group: :doc
 gem 'bootstrap-sass'
 gem 'devise', '~>3.5.2.rc2'
+gem 'paperclip', github: 'thoughtbot/paperclip'
 
 # Use ActiveModel has_secure_password
 # gem 'bcrypt', '~> 3.1.7'
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150831234052) do
+ActiveRecord::Schema.define(version: 20150901225349) do
 
   create_table "pins", force: :cascade do |t|
     t.string   "description"
@@ -21,9 +21,13 @@ ActiveRecord::Schema.define(version: 20150831234052) do
 
   create_table "posts", force: :cascade do |t|
     t.string   "description"
-    t.datetime "created_at",  null: false
-    t.datetime "updated_at",  null: false
+    t.datetime "created_at",         null: false
+    t.datetime "updated_at",         null: false
     t.integer  "user_id"
+    t.string   "image_file_name"
+    t.string   "image_content_type"
+    t.integer  "image_file_size"
+    t.datetime "image_updated_at"
   end
 
   add_index "posts", ["user_id"], name: "index_posts_on_user_id"
